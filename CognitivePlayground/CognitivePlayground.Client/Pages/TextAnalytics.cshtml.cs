3c97484d802b1475015d60695a0a128949c78eeb
miguel molina montilla <molidestroyer@hotmail.com>
2018-11-09 12:05:56 +0100

    Last changes

--- CognitivePlayground/CognitivePlayground.Client/Pages/TextAnalytics.cshtml.cs
+++ CognitivePlayground/CognitivePlayground.Client/Pages/TextAnalytics.cshtml.cs
@@ -1,9 +1,7 @@
 using CognitivePlayground.Shared;
 using Microsoft.AspNetCore.Blazor;
 using Microsoft.AspNetCore.Blazor.Components;
-using Microsoft.Azure.CognitiveServices.Language.TextAnalytics.Models;
 using Microsoft.JSInterop;
-using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -13,17 +11,20 @@ namespace CognitivePlayground.Client.Pages
     {
         protected string _text { get; set; } = "And when I think about Coretta Scott King, I think about the little girl who walked five miles to school on those rural Alabama roads and felt the heat of racism each day she passed the door of the Whites-only school, so much closer to home. It didn't matter, because she studied and succeeded and excelled beyond most of her classmates, Black and White. She earned a college degree, and an acceptance to a prestigious graduate school up North.";
         protected bool _loading { get; set; }
+        protected bool _dataLoaded { get; set; }
+
         [Inject]
         protected HttpClient Http { get; set; }
 
         public EntityLinkingResponse _resultEntities { get; set; } = new EntityLinkingResponse();
-        
+
         protected async void GetEntityLinkingResult()
         {
             _loading = true;
             _resultEntities = await Http.PostJsonAsync<EntityLinkingResponse>("api/SampleData/GetEntityLinkTextAnalytics", new EntityLinkingRequest() { Text = _text });
             await this.GenerateWordCloud();
             _loading = false;
+            _dataLoaded = true;
             this.StateHasChanged();
 
         }
